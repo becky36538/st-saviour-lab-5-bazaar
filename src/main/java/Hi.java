da7232c7604873b91a4fe19095063a2bd113800c
bmc2425 <recheriyan@stsaviour.org>
2026-02-13 16:37:55 -0500

    lab 5

--- src/main/java/Hi.java
+++ src/main/java/Hi.java
@@ -3,7 +3,7 @@ package main.java;
 public class Hi {
     public static void main(String[] args) {
         System.out.println("new dawn, ew day");
-        weave(vertical(11, 11));
+        weave(diagonal(11, 11));
     }
     public static void weave(String[][] rug) {
         // method to print 2d emooji arrays
@@ -16,15 +16,25 @@ public class Hi {
         }
     }
 
-    public static String[][] solid(int width, int height) {
+    public static String[][] diagonal(int width, int height) {
         String[][] rug = new String[width][height];
 
         for(int i = 0; i < rug.length; i++) {
             for(int j = 0; j < rug[i].length; j++) {
-                rug[i][j] = "🩷";
+                 if (i % 2 == 0 && j % 2 != 0) {
+                    rug[i][j] = "🪷";
+                } else if (i % 2 == 0 && j % 2 == 0){
+                    rug[i][j] = "🦋";
+                if (i % 2 != 0 && j % 2 == 0) {
+                    rug[i][j] = "🪷";
+                } else if (i % 2 != 0 && j % 2 != 0){
+                    rug[i][j] = "🦋";
             }
         }
 
         return rug;
+    }
+} return rug;
+
     }
 }
---
+++ src/main/java/Plaid.java
@@ -0,0 +1,34 @@
+
+
+public class Plaid {
+    public static void main(String[] args) {
+        System.out.println("new dawn, ew day");
+        weave(plaid(11, 11));
+    }
+    public static void weave(String[][] rug) {
+        // method to print 2d emooji arrays
+        for(int i = 0; i < rug.length; i++) {
+            for(int j = 0; j < rug[i].length; j++) {
+                System.out.print(rug[i][j]);
+            }
+            // break line at end of every row
+            System.out.println("");
+        }
+    }
+
+    public static String[][] plaid(int width, int height) {
+        String[][] rug = new String[width][height];
+
+        for(int i = 0; i < rug.length; i++) {
+            for(int j = 0; j < rug[i].length; j++) {
+                if (i % 2 == 0 && j % 2 == 0) {
+                    rug[i][j] = "🪷";
+                } else {
+                    rug[i][j] = "🌸";
+                }
+            }
+        }
+
+        return rug;
+    }
+}
