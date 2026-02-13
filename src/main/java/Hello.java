a7a655d6bcac69d3b46692eb2dc26fd1cd2d1add
bmc2425 <recheriyan@stsaviour.org>
2026-02-13 14:57:44 -0500

    lab 5

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,7 +3,7 @@ package main.java;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("new dawn, ew day");
-        weave(horizontal(11, 11));
+        weave(vertical(11, 11));
     }
     public static void weave(String[][] rug) {
         // method to print 2d emooji arrays
@@ -50,7 +50,7 @@ public class Hello {
 
         for(int i = 0; i < rug.length; i++) {
             for(int j = 0; j < rug[i].length; j++) {
-                if (i % 2 == 0) {
+                if (j % 2 != 0) {
                     rug[i][j] = "🦋";
                 } else {
                     rug[i][j] = "🪷";
